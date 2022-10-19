c23ffb25b8828fe1f638a99c254642b85a288586
Alparslan <alparslanaydgn02@gmail.com>
2022-10-19 16:28:54 +0300

    Alan hesaplama ve asal sayı hesaplama kodları eklendi

---
+++ areaOfTheCircle.js
@@ -0,0 +1,7 @@
+const arguments=process.argv.slice(1);
+function areaCircle(r) 
+{
+    console.log(r*r*3.14);
+}
+areaCircle(arguments[1] * 1);
+
---
+++ primeNumbers.js
@@ -0,0 +1,22 @@
+const arguments = process.argv.slice(2);
+
+
+function primeNumber(firstNumber,secondNumber) 
+{
+    for (let i = firstNumber; i <=secondNumber; i++) 
+    {
+        let control=true;
+        for (let j = 2; j <i ; j++) 
+        {
+            if (i%j==0) 
+            {
+                control=false;   
+            }         
+        }
+        
+        if (control==true) {
+            console.log(i);
+        }
+    }
+}
+primeNumber(arguments[0] * 1 , arguments[1] * 1);
